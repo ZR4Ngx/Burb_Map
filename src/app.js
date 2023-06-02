d49d41c4bc87fd0adb703b447ec6b550e2983444
ZR4Ngx <saran06020@gmail.com>
2023-06-03 01:53:09 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const port = 3000
+const port = process.env.PORT || 3000
 
 const loadMap = require("./mapLoader");
 const TICK_RATE = 30;
